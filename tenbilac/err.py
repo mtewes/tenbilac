aa53428724d8d48edf0953bbfbdc83a69a3e7663
mtewes <malte.tewes@gmail.com>
2015-06-24 14:10:43 +0200

    Moved from errorfct classes to functions

--- tenbilac/err.py
+++ tenbilac/err.py
@@ -8,29 +8,36 @@ import logging
 logger = logging.getLogger(__name__)
 
 
-class MSE:
-	"""
-	Standard MSE, simply treats realizations as if they were independent galaxies	
-	"""
 
-	def __call__(self, net, inputs, targets):
 	
-		return np.mean(np.square(net.run(inputs) - targets))
-		#return np.mean(np.square(net.run(inputs[0]) - targets))
-
-
-class MSB:
+def msb(predictions, targets):
 	"""
 	Mean square bias
 	
-	:param inputs: 3D array (realization, feature, galaxy)
-	:param targets: 2D array (feature, galaxy)
+	:param predictions: 3D array (realization, neuron, galaxy)
+	:param targets: 2D array (neuron, galaxy)
 	
 	"""
-
-	def __call__(self, net, inputs, targets):
-		return np.mean(np.square(np.mean(net.run(inputs), axis=0) - targets))
-		
+	if predictions.ndim == 3:
+		return np.mean(np.square(np.mean(predictions, axis=0) - targets))
+	else:
+		raise ValueError("Wrong pred shape")
 	
+	
+
+
+def mse(predictions, targets):
+	"""
+	Standard MSE (mean square error), simply treats multiple realizations as if they were independent galaxies	
+	
+	:param predictions: 2D array (neuron, galaxy) or 3D array (realization, neuron, galaxy)
+	:param targets: 2D array (neuron, galaxy)
+
+	"""
+	
+	# This same code works for 2D or 3D predictions:
+	return np.mean(np.square(predictions - targets))
+	
+	#return np.mean(np.square(net.run(inputs[0]) - targets))
 
 
