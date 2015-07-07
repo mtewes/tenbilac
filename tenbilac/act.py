5637e0ba387ba8851b79b53355bb492f5cfac709
mtewes <malte.tewes@gmail.com>
2015-07-07 11:23:23 +0200

    Added Tanh activation + rescaled Sig + make Tanh default

--- tenbilac/act.py
+++ tenbilac/act.py
@@ -14,8 +14,18 @@ class Sig:
 	"""
 
 	def __call__(self, x):
-		return 1 / (1 + np.exp(-x))
+		#return 1.0 / (1.0 + np.exp(-x))	# The actual sigmoid
+		return 2.0 / (1.0 + np.exp(-x)) - 1.0	# Making it even
+		
+		
 
+class Tanh:
+	"""
+	Tanh	
+	"""
+
+	def __call__(self, x):
+		return np.tanh(x)
 	
 	
 class Id:
@@ -25,3 +35,26 @@ class Id:
 	
 	def __call__(self, x):
 		return x
+
+
+if __name__ == "__main__":
+	
+	import matplotlib.pyplot as plt
+	
+	x = np.linspace(-5, 5, 100)
+	acts = [Sig, Tanh, Id]
+	
+	for act in acts:
+		f = act()
+		plt.plot(x, f(x), label=act.__name__)
+		
+	plt.xlabel(r"$x$")
+	plt.ylabel(r"$f(x)$")
+	plt.title("Activation functions")
+	plt.ylim(-1.2, 1.2)
+	plt.legend()
+	plt.grid()
+	plt.show()
+	
+	
+	
