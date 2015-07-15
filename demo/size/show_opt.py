262b6ef5b136af57cc89ca1697352f3eb721769a
mtewes <malte.tewes@gmail.com>
2015-07-15 10:16:45 +0200

    How it worked before Training class

--- demo/size/show_opt.py
+++ demo/size/show_opt.py
@@ -6,11 +6,13 @@ logging.basicConfig(level=logging.INFO)
 
 
 net = tenbilac.utils.readpickle("net_msb.pkl")
-tenbilac.plot.errorcurve(net)
+#tenbilac.plot.errorcurve(net)
+tenbilac.plot.paramscurve(net)
 
+"""
 net = tenbilac.utils.readpickle("net_avg.pkl")
 tenbilac.plot.errorcurve(net)
 
 net = tenbilac.utils.readpickle("net_mse.pkl")
 tenbilac.plot.errorcurve(net)
-
+"""
