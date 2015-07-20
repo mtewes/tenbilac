b5a3661cfb3bf18ddf15124f2984c6d668ac259c
mtewes <malte.tewes@gmail.com>
2015-07-20 11:49:10 +0200

    Paramsplot 2.0

--- tenbilac/plot.py
+++ tenbilac/plot.py
@@ -3,7 +3,11 @@ Plots directly related to tenbilac objects
 """
 
 import numpy as np
+import itertools
+import re
 import matplotlib.pyplot as plt
+import matplotlib.patches
+import matplotlib.lines
 
 import logging
 logger = logging.getLogger(__name__)
@@ -82,28 +86,77 @@ def paramscurve(train, filepath=None):
 	
 	"""
 	
-	fig = plt.figure(figsize=(10, 10))
-	
-	
+	# Getting the data 
 	optitparams = np.array(train.optitparams)
 	optiterrs_train = np.array(train.optiterrs_train)
 	optiterrs_val = np.array(train.optiterrs_val)
 	optits = np.arange(len(train.optitparams)) + 1
+	optbatchchangeits = getattr(train, "optbatchchangeits", [])
 	
+	# The cpu durations
+	optittimes = np.array(train.optittimes)
+	cumoptittimes = np.cumsum(optittimes)
+	assert cumoptittimes.size == optittimes.size
+	
+	if optittimes.size > 10:
+		labelstep = int(float(optittimes.size)/10.0)
+		timeindices = range(labelstep, optittimes.size, labelstep)
+	else:
+		timeindices = []
+	
+	
+	paramlabels = train.net.get_paramlabels()
+	# Now we find out how many layers we have, to be able to properly attribute colors
+	#layernames = [re.match("layer-(.*)_(.*)", label).group(1) for label in paramlabels]
+	#difflayernames = list(set(layernames))
+	#colors = itertools.cycle(["r", "black", "b", "g"])	
+	
+	
+	fig = plt.figure(figsize=(10, 10))
 	ax = plt.subplot(2, 1, 1)
-	ax.plot(optits, optiterrs_train, ls="-", color="black")
-	ax.plot(optits, optiterrs_val, ls="--", color="red")
+	
+	for optbatchchangeit in optbatchchangeits:
+		ax.axvline(optbatchchangeit, color="gray")
+
+	ax.plot(optits, optiterrs_train, ls="-", color="black", label="Training batch")
+	ax.plot(optits, optiterrs_val, ls="--", color="red", label="Validation set")
+	
+	for i in timeindices:
+		ax.annotate("{0:.1f}".format(cumoptittimes[i]), xy=(optits[i], optiterrs_val[i]), xytext=(0, 10), textcoords='offset points')
 	
 	ax.set_yscale('log')
 	ax.set_xlabel("Iteration")
 	ax.set_ylabel("Cost function value ({0})".format(train.errfctname))
+	ax.legend()
+	ax.set_title(train.title())
 	
 	ax = plt.subplot(2, 1, 2)
+	
 	assert optitparams.shape[1] == train.net.nparams()
 	for paramindex in range(train.net.nparams()):
-		ax.plot(optits, optitparams[:,paramindex])
+		label = paramlabels[paramindex]
+		if label.endswith("_bias"):
+			ls = "--"
+		elif label.endswith("_weight"):
+			ls = "-"
+		layername = re.match("layer-(.*)_(.*)", label).group(1)
+		if layername == "o":
+			color="black"
+		else:
+			color="red"
+		
+		pla = ax.plot(optits, optitparams[:,paramindex], ls=ls, color=color)
 	ax.set_xlabel("Iteration")
 	ax.set_ylabel("Network parameter value")
+	
+	# Now creating the legend
+	
+	black_patch = matplotlib.patches.Patch(color='black', label='Output layer')
+	red_patch = matplotlib.patches.Patch(color='red', label='Hidden layers')
+	line = matplotlib.lines.Line2D([], [], color='black', marker='', ls="-", label='Weight')
+	dashed = matplotlib.lines.Line2D([], [], color='black', marker='', ls="--", label='Bias')
+	
+	ax.legend(handles=[line, dashed, black_patch, red_patch])
 	
 	plt.tight_layout()
 	plt.show()	
