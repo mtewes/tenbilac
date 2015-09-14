20115cb6c0e45a339c7d44d5d37dbc0eb17d2b9a
mtewes <malte.tewes@gmail.com>
2015-09-14 11:48:14 +0200

    Adding 2dsize demo

---
+++ demo/2dsize/evoplot.py
@@ -0,0 +1,30 @@
+import numpy as np
+import tenbilac
+import matplotlib.pyplot as plt
+
+import logging
+logging.basicConfig(level=logging.DEBUG)
+
+
+data = tenbilac.utils.readpickle("data.pkl")
+
+
+train = tenbilac.utils.readpickle("train_msbw.pkl")
+
+fig = plt.figure(figsize=(10, 10))
+ax = plt.subplot(3, 1, 1)
+
+tenbilac.plot.errevo(ax, train, showtimes=True)
+
+ax = plt.subplot(3, 1, 2)
+tenbilac.plot.paramsevo(ax, train, wnetpart="o")
+
+ax = plt.subplot(3, 1, 3)
+tenbilac.plot.paramsevo(ax, train, wnetpart="w")
+
+
+plt.tight_layout()
+	
+plt.show()
+
+
