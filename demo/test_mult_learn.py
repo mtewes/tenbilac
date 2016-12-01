0db1c19848678c0aea2514897015f2fd9165a10f
mtewes <malte.tewes@gmail.com>
2016-12-01 11:31:25 +0100

    Demo of mult layer and sa1 normer

--- demo/test_mult_learn.py
+++ demo/test_mult_learn.py
@@ -18,8 +18,8 @@ ncas = 1000
 # We prepare the data:
 
 # Range of x and y
-xs = np.random.uniform(0.1, 1, ncas)
-ys = np.random.uniform(0.1, 1, ncas)
+xs = np.random.uniform(-5, 5, ncas)
+ys = np.random.uniform(-10, 30, ncas)
 
 zs = xs * ys 
 
@@ -40,29 +40,47 @@ targets = np.array([zs])
 #print targets.shape
 
 
-#inputnormer = tenbilac.data.Normer(inputs, type="01")
-#inputs = inputnormer(inputs) + 0.01
+
+inputnormer = tenbilac.data.Normer(inputs, type="sa1")
+inputs = inputnormer(inputs)
+targetnormer = tenbilac.data.Normer(targets, type="sa1")
+targets = targetnormer(targets)
+
+print inputnormer
+print targetnormer
+
 
 dat = tenbilac.data.Traindata(inputs=inputs, targets=targets)
 
-net = tenbilac.net.Net(ni=2, nhs=[-1], no=1, actfctname="iden", oactfctname="iden", inames=["x", "y"], onames=["z"])
+net = tenbilac.net.Net(ni=2, nhs=[-1], no=1, actfctname="iden", oactfctname="iden", multactfctname="iden", inames=["x", "y"], onames=["z"])
 #net = tenbilac.net.Net(ni=2, nhs=[5, 5], inames=["x", "y"], onames=["z"])
 
-#print net.report()
-net.addnoise()
+net.setidentity()
 
-training = tenbilac.train.Training(net, dat, errfctname="mse", autoplot=False, autoplotdirpath="plots")
+# The exact solution:
+#net.layers[0].weights[0,1] = 1.0
+#net.layers[1].weights[0] = ((inputnormer.b[0]*inputnormer.b[1])/targetnormer.b[0])
+print net.report()
 
-training.bfgs(maxiter=1000, gtol=1e-20)
+#net.addnoise(multwscale=0.01, wscale=0.01, bscale=0.01)
 
 print net.report()
 
-outs = net.predict(inputs)
+training = tenbilac.train.Training(net, dat, errfctname="mse", autoplot=False, autoplotdirpath="plots")
 
+training.bfgs(maxiter=50, gtol=1e-8)
+
+
+print net.report()
+
+outs = targetnormer.denorm(net.predict(inputs))
+#outs = net.predict(inputs)
 
 assert nrea==1
 
-zerrs = (outs[0] - targets)[0]
+zerrs = (outs[0] - np.array([zs]))[0]
+print np.mean(zerrs)
+print np.std(zerrs)
 
 import matplotlib.pyplot as plt
 
