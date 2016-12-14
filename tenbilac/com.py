505fec4768c6e8499f54ef9bd026d95e01f2f97a
mtewes <malte.tewes@gmail.com>
2016-12-14 14:17:11 +0100

    Adding com.py

---
+++ tenbilac/com.py
@@ -0,0 +1,20 @@
+"""
+The entry point to Tenbilac: functions defined here take care of running committees
+(i.e., ensembles of networks) and communicating with config files. Hence the name com.
+"""
+
+
+
+def setup():
+
+
+def summary(trainings):
+	"""Analyses how the trainings of a committee went. Logs info and calls a checkplot.
+	
+	:param trainings: list of training instances
+	
+	
+	This could be a method of a ComTraining, but for increased flexibility we leave it 
+	"""
+
+
